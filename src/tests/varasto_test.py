efb6ef861aa463abcde678292b704113de6e93f3
Mäntylä Kiia <mantylak@cubbli24-gold.cs.helsinki.fi>
2025-11-02 12:14:31 +0200

    Korjattu testit näkyviksi

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,41 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_negatiivinen_tilavuus_nollataan(self):
+        v = Varasto(-5)
+        self.assertAlmostEqual(v.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo_nollataan(self):
+        v = Varasto(10, -2)
+        self.assertAlmostEqual(v.saldo, 0)
+
+    def test_alkusaldo_yli_tilavuuden_rajoitetaan(self):
+        v = Varasto(10, 15)
+        self.assertAlmostEqual(v.saldo, 10)
+
+    def test_lisaa_negatiivinen_maara_ei_muuta_saldoa(self):
+        self.varasto.lisaa_varastoon(-3)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisaa_yli_tilavuuden_tayttaa_varaston(self):
+        self.varasto.lisaa_varastoon(12)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_negatiivinen_maara_palauttaa_nolla(self):
+        self.varasto.lisaa_varastoon(5)
+        tulos = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(tulos, 0)
+        self.assertAlmostEqual(self.varasto.saldo, 5)
+
+    def test_ota_enemman_kuin_on_tyhjentaa_varaston(self):
+        self.varasto.lisaa_varastoon(5)
+        tulos = self.varasto.ota_varastosta(20)
+        self.assertAlmostEqual(tulos, 5)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_str_palauttaa_oikean_merkkijonon(self):
+        self.varasto.lisaa_varastoon(5)
+        s = str(self.varasto)
+        self.assertIn("saldo = 5", s)
+        self.assertIn("vielä tilaa 5", s)
