6f20a4e427e579581958f2e86cb5e6ccb8b126d5
Mäntylä Kiia <mantylak@cubbli24-gold.cs.helsinki.fi>
2025-11-06 18:51:57 +0200

    pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,22 +1,20 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
+        self.tilavuus = self._aseta_tilavuus(tilavuus)
+        self.saldo = self._aseta_saldo(alku_saldo)
+
+    def _aseta_tilavuus(self, tilavuus):
         if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
+            return tilavuus
+        return 0.0
 
-        if alku_saldo < 0.0:
-            # virheellinen, nollataan
-            self.saldo = 0.0
-        elif alku_saldo <= tilavuus:
-            # mahtuu
-            self.saldo = alku_saldo
-        else:
-            # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+    def _aseta_saldo(self, saldo):
+        if saldo < 0.0:
+            return 0.0
+        if saldo <= self.tilavuus:
+            return saldo
+        return self.tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
